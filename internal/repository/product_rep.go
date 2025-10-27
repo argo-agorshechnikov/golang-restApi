b754e8136522c069974d6ff681c557328807cc95
agorshechnikov <arkadiy.gorshechnikov@yandex.com>
2025-10-28 02:55:12 +0500

    create model and rep product

---
+++ internal/repository/product_rep.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/argo-agorshechnikov/golang-restApi/internal/models"
+)
+
+type ProductRep struct {
+	db *sql.DB
+}
+
+func NewProductRep(connStr string) (*ProductRep, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &ProductRep{db: db}, nil
+}
+
+func (r *ProductRep) CreateProductRep(product *models.Product) error {
+
+	query := "INSERT INTO products(id, name, description) VALUES ($1, $2, $3)"
+
+	_, err := r.db.Exec(query, product.ID, product.Name, product.Description)
+	return err
+}
+
+func (r *ProductRep) GetProductById(id string) (*models.Product, error) {
+	var product models.Product
+
+	query := "SELECT id, name, desctiptions FROM products WHERE id = $1"
+	err := r.db.QueryRow(query, id).Scan(&product.ID, &product.Name, &product.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+
+}
