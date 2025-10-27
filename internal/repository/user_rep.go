994ce095a1d8cdc231cc98f983e73b61dcbf8539
agorshechnikov <arkadiy.gorshechnikov@yandex.com>
2025-10-27 23:23:37 +0500

    add user model and user_rep

---
+++ internal/repository/user_rep.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/argo-agorshechnikov/golang-restApi/internal/models"
+
+	_ "github.com/lib/pq"
+)
+
+type UserRep struct {
+	db *sql.DB
+}
+
+func NewUserRep(connStr string) (*UserRep, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &UserRep{db: db}, nil
+}
+
+func (r *UserRep) Create(user *models.User) error {
+
+	query := `INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)`
+
+	_, err := r.db.Exec(query, user.ID, user.Name, user.Email, user.Password)
+	return err
+}
+
+func (r *UserRep) GetUserByID(id string) (*models.User, error) {
+	var user models.User
+	query := `SELECT id, name, email FROM users WHERE id = $1`
+	err := r.db.QueryRow(query, id).Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
