b7cd9f041a4fc5830885e97f25040f579c39d613
agorshechnikov <arkadiy.gorshechnikov@yandex.com>
2025-11-01 01:00:30 +0500

    change user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Password string `json:"-"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
