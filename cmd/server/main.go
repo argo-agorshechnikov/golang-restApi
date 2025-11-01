081f1aa55c7d39e7254df6fa119d5611ccfe4d4f
agorshechnikov <arkadiy.gorshechnikov@yandex.com>
2025-11-01 13:28:52 +0500

    change main.go for product

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -22,12 +22,22 @@ func main() {
 		log.Fatalf("Failed to connect to DB: %v", err)
 	}
 
+	productRep, err := repository.NewProductRep(connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to BD: %v", err)
+	}
+
 	userService := service.NewUserService(userRepo)
 	userHandler := handlers.NewUserHandler(userService)
+
+	productService := service.NewProductService(productRep)
+	productHandler := handlers.NewProductHandler(productService)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/users", userHandler.CreateUserHand)
 	mux.HandleFunc("/user", userHandler.GetUserByIdHand)
+	mux.HandleFunc("/products", productHandler.CreateProductHandler)
+	mux.HandleFunc("product", productHandler.GetProductByIdHandler)
 
 	addr := ":8080"
 	log.Printf("Server running")
